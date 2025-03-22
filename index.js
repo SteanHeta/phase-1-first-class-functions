d7975b2966e43befffc5849491b0a08773c44ca2
Stean <stean.ngugi@student.moringaschool.com>
2025-03-22 23:55:12 +0300

    creates index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const { returns } = require("chai-spies");
+
+function receivesAFunction(callback) {
+    callback();
+}
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("I'm a named function!");
+        
+    };
+}
+function returnsAnAnonymousFunction() {
+    return function () {
+        console.log("I'm an anonymous function!");
+        
+    };
+}
